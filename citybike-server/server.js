08eee37e53d1101c0903331a8f91e132060789a7
stivendiaz <stivendiazagudelo@gmail.com>
2021-07-14 01:21:09 -0500

    feat: emit bike stations data using socketio

--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const axios = require("axios");
+
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
 
 
@@ -20,14 +22,23 @@ io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
   console.log('New connection ' + socketId + ' from ' + clientIp);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getAndEmitData(socket), 3000); // refresh every 3 seconds
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
+const getAndEmitData = async (socket) => {
+  try {
+    const res = await axios.get(citybikeurl);
+    const { stations } = res.data.network;
+    socket.emit('bikes-socket', stations);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
 
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
+server.listen(port, () => console.log(`Server listening on port: ${port}`));
